d45a26b6e8be853ba761c660adfd5ec3341e5532
Garth Zeglin <garthz@cmu.edu>
2016-01-19 15:58:43 -0500

    Adding a package for Grasshopper-specific Python modules.
    Including a list-to-tree conversion.

---
+++ python/ghutil/__init__.py
@@ -0,0 +1,7 @@
+# Init file for the ghutil package.
+
+# Make all functions from particular modules available in the top-level namespace.
+from trees import *
+
+
+
---
+++ python/ghutil/trees.py
@@ -0,0 +1,25 @@
+"""\
+ghutil.trees
+
+Utility functions related to creating Data Trees in Grasshopper.
+"""
+
+#================================================================
+# from https://gist.github.com/piac/ef91ac83cb5ee92a1294
+def list_to_tree(input, none_and_holes=True, source=[0]):
+    """Transforms nestings of lists or tuples to a Grasshopper DataTree"""
+    from Grasshopper import DataTree as Tree
+    from Grasshopper.Kernel.Data import GH_Path as Path
+    from System import Array
+    def proc(input,tree,track):
+        path = Path(Array[int](track))
+        if len(input) == 0 and none_and_holes: tree.EnsurePath(path); return
+        for i,item in enumerate(input):
+            if hasattr(item, '__iter__'): #if list or tuple
+                track.append(i); proc(item,tree,track); track.pop()
+            else:
+                if none_and_holes: tree.Insert(item,path,i)
+                elif item is not None: tree.Add(item,path)
+    if input is not None: t=Tree[object]();proc(input,t,source[:]);return t
+
+#================================================================
