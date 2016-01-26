f51a2781e6058fd38737f7685d3278fb05bf89e5
Garth Zeglin <garthz@cmu.edu>
2016-01-26 09:46:00 -0500

    Adding a Plane recording function; adding some CSV data tree unpacking and playback.

---
+++ Grasshopper/MocapDemo/demo_plane_recorder.py
@@ -0,0 +1,49 @@
+﻿# demo_plane_recorder.py - the plane data recording script in the ghpython object in MocapDemo.gh.
+#
+# inputs
+#  input    - single Plane object
+#  capture  - Boolean indicating the object should be added to the set
+#  clear    - Boolean indicating the the data set should be erased
+
+# other ideas:
+#   path       - string with the full path to the CSV file
+#   selection  - currently selected set
+
+# outputs
+#   out      - debugging text stream
+#   planes   - list of Plane objects
+
+# use RhinoCommon API
+import Rhino
+
+# use persistent state context
+import scriptcontext
+
+################################################################
+class PlaneRecorder(object):
+    def __init__(self):
+        self.planes = []
+        return
+
+    def add_plane(self, plane):
+        self.planes.append(plane)
+        return
+
+################################################################
+if clear:
+    # create an empty data recorder
+    recorder = PlaneRecorder()
+    scriptcontext.sticky['plane_recorder'] = recorder
+
+else:
+    # fetch or create a persistent data recorder
+    recorder = scriptcontext.sticky.get('plane_recorder')
+    if recorder is None:
+        recorder = PlaneRecorder()
+        scriptcontext.sticky['plane_recorder'] = recorder
+
+if capture:
+    recorder.add_plane(input)
+
+planes = recorder.planes
+print "Currently holding %d planes." % len(planes)
