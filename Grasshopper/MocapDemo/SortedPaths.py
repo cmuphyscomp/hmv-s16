d979cda7f27f75c741259afa9db6f07c703aa7b5
Garth Zeglin <garthz@cmu.edu>
2016-02-18 16:57:53 -0500

    Adding code for small utility block to sort branch names and emit branch paths in a predictable order.

---
+++ Grasshopper/MocapDemo/SortedPaths.py
@@ -0,0 +1,15 @@
+﻿# SortedPaths.py - contents of the ghpython object
+# inputs
+#  names - list of body names from a mocap stream
+
+# outputs
+#  paths - list of data tree paths in order sorted by name
+#  Pn    - individual path
+
+sorted_names = sorted(names)
+indices = [names.index(name) for name in sorted_names]
+paths = ["0;%d" % index for index in indices]
+
+p1 = paths[0]
+p2 = paths[1]
+p3 = paths[2]
