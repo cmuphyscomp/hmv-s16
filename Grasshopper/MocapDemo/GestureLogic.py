6568548220097bc6dd5d9b6dd8d3c9b43f5ec66e
Garth Zeglin <garthz@cmu.edu>
2016-03-29 11:10:01 -0400

    Restructured GestureLogic into class methods for maintainability.

--- Grasshopper/MocapDemo/GestureLogic.py
+++ Grasshopper/MocapDemo/GestureLogic.py
@@ -1,19 +1,48 @@
-﻿# GestureLogic - state machine for interface logic
-
-# FIXME: this is out of date
-# inputs
-#  name   - name token string for sticky
-#  reset  - bool to reset to lowest value
-#  sample - None or relative sample index of detected gesture event
-#  cursor - list of Planes with recent cursor object trajectory
-#  poses  - list of Planes saved at gesture events
-# Note: cursor and poses must have 'List Access' set
+﻿# GestureLogic - state machine for interface logic for the gesture-based editing.
 #
-# outputs
-#  moving - Boolean to indicate when selection is actively in motion
-#  start  - None or Plane to indicate origin for transformation
-#  end    - None or Plane to indicate target for transformation
+# This encompasses all the logic for the editor which is easier to write in
+# Python than Grasshopper objects.  Only one EditorLogic instance is expected to
+# exist since it holds and tracks user inputs.
+#
+# Objectives for this block:
+#
+# 1. Only hold transient user state.  All persistent data is read from the
+#    RhinoDoc, manipulated in a transient way, and then written back to the
+#    RhinoDoc or discarded.
+#
+# 2. Emit signals to read and write the model rather than directly manipulate
+#    the RhinoDoc.  This does increase the number of I/O variables, but
+#    is intended to make the operation easier to observe, debug, and extend.
+# 
+# inputs:
+#  name    - name token string for sticky
+#  reset   - bool to reset to lowest value
+#  gesture - None or relative sample index (integer) of detected gesture event
+#  cursor  - list of Planes with recent cursor object trajectory
+#  poses   - list of Planes saved at gesture events
+#  mode
+#  update
+#  selection
+#  selguids
+#  setselect
+#  clear
+#  used_names
+#  all_names
 
+# Note: the following must have 'List Access' set: cursor, poses, selection, selguids, used_names, all_names
+#
+# outputs:
+#  out    - log string output for display; the log is persistent to reduce the amount of flickering
+#  add
+#  move
+#  names
+#  newloc
+#  status
+#  objects
+#  guids
+#  xform
+
+################################################################
 import scriptcontext as sc
 import Rhino
 
@@ -30,7 +59,7 @@ class EditorLogic(object):
         self.docguids = None
         self.transform = None  # coordinate transformation for group edits
         self.motion    = None  # coordinate transformation for group edits
-        self.log       = ["Editor initialized."]    # list of string in which to accumulate messages for output
+        self.log       = ["Editor initialized."]    # list of strings in which to accumulate messages for output
         return
 
     def add_new_object_pose(self, plane):   
@@ -86,17 +115,118 @@ class EditorLogic(object):
         new_name = [name for name in self.unused_names][0]
         self.unused_names.remove(new_name)
         return new_name
-        
-################################################################
-editor = sc.sticky.get(name)
 
+    def manage_user_selection(self, setselect, selection, selguids, all_names, used_names):
+        """Process the user 'Set Selection' input, updating the editor state for any new
+        objects and names as needed."""
+        
+        if setselect != self._last_setselect:
+            # debounce input to just trigger once
+            self._last_setselect = setselect
+            if setselect == True:
+                self.selection = selection
+                self.docguids = selguids
+
+                # reset the pick and place state
+                self.attached = False
+                self.transform = None
+
+                self.logprint("Selection set with %d objects." % len(selection))
+                self.set_namesets(all_names, used_names)
+
+    #================================================================
+    def update_tap_create_mode(self, gesture, cursor):
+        """Update state for the 'tap create' mode in which gestures create individual cards.
+        gesture - the integer gesture sample index or None
+        cursor  - the list of recent cursor poses
+
+        returns newlog, names, add
+        """
+
+        # default outputs
+        names, add = None, None
+        
+        # tap create mode: each gesture creates a new object
+        if gesture is not None:
+            self.logprint("Gesture detected with sample offset %d." % gesture)
+            # gesture is an integer sample where zero is the most recent pose;
+            # index the current cursor poses from the end to select the correct
+            # pose
+            self.add_new_object_pose(cursor[gesture-1])
+
+        if clear == True:
+            self.logprint( "Abandoning editor changes (dropping new object poses).")
+            self.clear_edits()         
+
+        # by default, always emit the new poses so they can be visualized
+        newloc = self.new_object_poses
+
+        if update == True:
+            self.logprint("Writing new objects to RhinoDoc: %s" % self.new_object_names)
+            names = self.new_object_names
+            add = True
+            self.clear_edits() # note: the current list has already been emitted, this just resets the buffer
+
+        return newloc, names, add
+
+    #================================================================
+    def update_block_move_mode(self, gesture, cursor, poses, update, clear):
+        """Update state for the 'block move' mode in which each gesture alternately
+        attaches or detaches the selection from the cursor.
+
+        returns objects, guids, xform, move
+        """
+
+        # set default outputs
+        objects = self.selection
+        guids = self.docguids
+        move = False
+        
+        motion = Rhino.Geometry.Transform(1) # motion transform is identity value by default
+
+        if clear == True:
+            self.logprint("Abandoning editor changes (clearing movement).")
+            self.transform = None
+            
+        # detect the singular gesture events (for now, a flick of the wand)
+        if gesture is not None:
+            # if we are ending a motion segment, save the most recent transformation as the new base transform
+            if self.attached:
+                self.transform = self.transform * self.motion
+                self.logprint("Motion ended, new transform saved.")
+            else:
+                self.logprint("Motion beginning.")
+                
+            # toggle the 'attached' state
+            self.attached = not self.attached
+            
+        if self.attached:
+            if len(poses) > 0 and len(cursor) > 0:
+                # compute a tranform the from most recent saved pose to the newest cursor position
+                motion = Rhino.Geometry.Transform.PlaneToPlane(poses[-1], cursor[-1])
+        
+        # compute an output transformation from the accumulated transform plus any transient movement
+        if self.transform is None:
+            self.transform = Rhino.Geometry.Transform(1) # identity
+        xform = self.transform * motion
+        self.motion = motion
+
+        if update == True:
+            self.logprint("Updating RhinoDoc selection with new poses.")
+            move = True
+            self.clear_edits()
+
+        return objects, guids, xform, move
+
+
+################################################################
+# create or re-create the editor state as needed
+editor = sc.sticky.get(name)
 if editor is None or reset:
     editor = EditorLogic()
     sc.sticky[name] = editor
 
-# set default outputs
-start  = None
-end    = None
+# set default output values
 add    = False
 move   = False
 names  = None
@@ -111,80 +241,15 @@ if reset:
     
 else:
     # for all modes, record the set of selected objects when indicated
-    if setselect != editor._last_setselect:
-        # debounce input to just trigger once
-        editor._last_setselect = setselect
-        if setselect == True:
-            editor.selection = selection
-            editor.docguids = selguids
-    
-            # reset the pick and place state
-            editor.attached = False
-            editor.transform = None
-            
-            editor.logprint("Selection set with %d objects." % len(selection))
-            editor.set_namesets(all_names, used_names)
-        
+    editor.manage_user_selection(setselect, selection, selguids, all_names, used_names)
+
+    # handle the state update for each individual mode
     if mode == 1:
-        # tap create mode: each gesture creates a new object
-        if gesture is not None:
-            editor.logprint("Gesture detected with sample offset %d." % gesture)
-            # gesture is an integer sample where zero is the most recent pose;
-            # index the current cursor poses from the end to select the correct
-            # pose
-            editor.add_new_object_pose(cursor[gesture-1])
-
-        if clear == True:
-            editor.logprint( "Abandoning editor changes (dropping new object poses).")
-            editor.clear_edits()         
-
-        # by default, always emit the new poses so they can be visualized
-        newloc = editor.new_object_poses
-
-        if update == True:
-            editor.logprint("Writing new objects to RhinoDoc: %s" % editor.new_object_names)
-            names = editor.new_object_names
-            add = True
-            editor.clear_edits() # note: the current list has already been emitted, this just resets the buffer
-
-    elif mode == 3:
-        # pick and move: each gesture toggles whether selected objects are attached to cursor or not
- 
-        objects = editor.selection
-        guids = editor.docguids
-        motion = Rhino.Geometry.Transform(1) # motion transform is identity value by default
-        if clear == True:
-            editor.logprint("Abandoning editor changes (clearing movement).")
-            editor.transform = None
-            
-        # detect the singular gesture events (for now, a flick of the wand)
-        if gesture is not None:
-            # if we are ending a motion segment, save the most recent transformation as the new base transform
-            if editor.attached:
-                editor.transform = editor.transform * editor.motion
-                editor.logprint("Motion ended, new transform saved.")
-            else:
-                editor.logprint("Motion beginning.")
-                
-            # toggle the 'attached' state
-            editor.attached = not editor.attached
-            
-        if editor.attached:
-            if len(poses) > 0 and len(cursor) > 0:
-                # compute a tranform the from most recent saved pose to the newest cursor position
-                motion = Rhino.Geometry.Transform.PlaneToPlane(poses[-1], cursor[-1])
+        newloc, names, add = editor.update_tap_create_mode(gesture, cursor)
         
-        # compute an output transformation from the accumulated transform plus any transient movement
-        if editor.transform is None:
-            editor.transform = Rhino.Geometry.Transform(1) # identity
-        xform = editor.transform * motion
-        editor.motion = motion
-
-        if update == True:
-            editor.logprint("Updating RhinoDoc selection with new poses.")
-            move = True
-            editor.clear_edits()
-
+    elif mode == 3:
+        objects, guids, xform, move = editor.update_block_move_mode(gesture, cursor, poses, update, clear)
+        
     # emit terse status for remote panel
     status = "M:%s C:%d P:%d N:%d" % (editor.attached, len(cursor), len(poses), len(editor.new_object_poses))
 
